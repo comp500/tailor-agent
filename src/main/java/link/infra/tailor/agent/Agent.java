316f6db1e5325f66990e6ad6348e574372fd38d2
comp500 <comp500@users.noreply.github.com>
2020-06-06 18:43:53 +0100

    Add Hotloading TCP server

--- src/main/java/link/infra/tailor/agent/Agent.java
+++ src/main/java/link/infra/tailor/agent/Agent.java
@@ -1,14 +1,21 @@
 package link.infra.tailor.agent;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Agent implements ClassFileTransformer {
 	public static void premain(String agentArgs, Instrumentation inst) {
@@ -22,6 +29,13 @@ public class Agent implements ClassFileTransformer {
 		inst.addTransformer(new Agent(), true);
 
 		// TODO: filter classes?
+		retransformExisting(inst);
+
+		// TODO: listen on TCP, wait for reload command
+		startHotloadThread(inst);
+	}
+
+	private static void retransformExisting(Instrumentation inst) {
 		// Ensure that all classes are dumped, by retransforming existing loaded classes
 		Class<?>[] classes = inst.getAllLoadedClasses();
 		try {
@@ -29,8 +43,52 @@ public class Agent implements ClassFileTransformer {
 				inst.retransformClasses(classes);
 			}
 		} catch (UnmodifiableClassException ignored) {}
+	}
 
-		// TODO: listen on TCP, wait for reload command
+	private static void startHotloadThread(Instrumentation inst) {
+		new Thread(() -> {
+			try {
+				ServerSocket server = new ServerSocket(19303);
+				while (true) {
+					Socket clientSocket = server.accept();
+					new Thread(() -> {
+						List<Class<?>> classReloadList = new ArrayList<>();
+						Class<?>[] classes = inst.getAllLoadedClasses();
+						try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+							String classToReload;
+							while ((classToReload = reader.readLine()) != null) {
+								// TODO: lambda names are dynamic?
+								boolean matched = false;
+								for (Class<?> clazz : classes) {
+									if (classToReload.equals(clazz.getName())) {
+										classReloadList.add(clazz);
+										matched = true;
+										break;
+									}
+								}
+								if (!matched) {
+									System.out.println("[tailor-agent] Failed to match class " + classToReload);
+								}
+							}
+						} catch (IOException e) {
+							// TODO: better error handling
+							e.printStackTrace();
+						}
+						String[] classReloadNames = classReloadList.stream().map(Class::getName).toArray(String[]::new);
+						System.out.println("[tailor-agent] Reloading classes: " + Arrays.toString(classReloadNames));
+						try {
+							inst.retransformClasses(classReloadList.toArray(new Class<?>[0]));
+						} catch (UnmodifiableClassException e) {
+							// TODO: better error logging
+							e.printStackTrace();
+						}
+					}, "tailor-agent Hotload socket handler").start();
+				}
+			} catch (IOException e) {
+				// TODO: better error logging
+				e.printStackTrace();
+			}
+		}, "tailor-agent Hotload server").start();
 	}
 
 	private final Path dumpPath;
